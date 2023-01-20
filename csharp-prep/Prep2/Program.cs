de5ccb0f992c5329aa89f22fe541d7e9201d07e5
ihuomajohn <ano22002@byui.edu>
2023-01-20 17:15:02 +0100

    C# Prep 2 question 1 completed

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter your grade percentage ");
+        string userInput = Console.ReadLine();
+
+        int x = int.Parse(userInput);
+        int y = 90;
+        int z = 80;
+        int m = 70;
+        int n = 60;
+        
+        if (x >= y)
+        {
+            Console.Write("Your grade is A");
+        }
+        else if (x >= z && x < y)
+        {
+            Console.Write("Your grade is B");
+        } 
+        else if (x >= m && x < z)
+        {
+            Console.Write("Your grade is C");
+        }
+        else if (x >= n && x < m)
+        {
+            Console.Write("Your grade is D");
+        }
+        else
+        {
+            Console.Write("Your grade is F");
+        }
     }
 }
