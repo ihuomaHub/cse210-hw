439f5613674a3f69d6a450760dbb67a7e2b113ab
ihuomajohn <ano22002@byui.edu>
2023-01-20 17:19:32 +0100

    C# Prep 1 question completed

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -8,7 +8,6 @@ class Program
         string first = Console.ReadLine();
         Console.WriteLine("What is your last name?");
         string last = Console.ReadLine();
-
         Console.WriteLine($"Your name is {first}, {last} {first}.");
     }
 }
