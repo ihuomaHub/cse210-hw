1a548fe30ef3f820dace9fc6e0f8008871ef77f8
ihuomajohn <ano22002@byui.edu>
2023-01-27 14:52:31 +0100

    C# Prep 4 core and stretch requirements complete

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -5,12 +5,14 @@ class Program
 {
     static void Main(string[] args)
     {
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        
         List<int> numbers = new List<int>();
         
         int userNumber = -1;
         while (userNumber != 0)
         {
-            Console.Write("Enter a list of numbers, type 0 when finished: ");
+            Console.Write("Enter number: ");
             
             string userResponse = Console.ReadLine();
             userNumber = int.Parse(userResponse);
@@ -21,19 +23,19 @@ class Program
             }
         }
 
-        // Part 1: Compute the sum
+        // Part 1: Computing the sum
         int sum = 0;
         foreach (int number in numbers)
         {
             sum += number;
         }
 
-        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The sum of the numbers in the list is: {sum}");
 
         // Part 2: Computing the average
       
         float average = ((float)sum) / numbers.Count;
-        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The average number in the list is: {average}");
 
         // Part 3: Finding the max
        
@@ -47,6 +49,45 @@ class Program
             }
         }
 
-        Console.WriteLine($"The max is: {max}");
+        Console.WriteLine($"The maximum number in the list is: {max}");
+
+        // The Stretch Activity: Finding the minimum number
+
+        int min = numbers[0];
+        
+        foreach (int number in numbers)
+        {
+            if (number < min)
+            {
+                min = number;
+            }
+        
+        }
+
+        Console.WriteLine($"The minimum number in the list is: {min}");
+        Console.WriteLine($"The total number entered is {numbers.Count}");
+        
+        // The Stretch Activity: Finding the minimum positive number
+
+        int minPositive = numbers[0];
+        foreach (int number in numbers)
+        {
+            if (number > 0 && number < minPositive)
+            {
+                minPositive = number;
+            }
+        }
+        Console.WriteLine($"The minimum positive number in the list is: {minPositive}");
+
+        // The Stretch Activity: Sorting the numbers entered by the user
+        Console.WriteLine("The Sorted list is:");
+        numbers.Sort();
+        foreach (int number in numbers)
+        
+        {
+            Console.WriteLine($"{number}");  
+        }
+       
+
     }
 }
